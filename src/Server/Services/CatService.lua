f8f6db67cfe54d00de88d7169ec05cb3029f3511
Igor Dvorkin <idvorkin@gmail.com>
2020-09-07 08:51:25 -0700

    Chekcpoint!

--- src/Server/Services/CatService.lua
+++ src/Server/Services/CatService.lua
@@ -1,34 +1,31 @@
--- Echo Service
+-- Cat Service
 -- Username
--- September 6, 2020
+-- September 7, 2020
 
-
-
-local EchoService = {Client = {}}
-
-function MakeAClone(me)
-    local clone = me:Clone()
+function MakeAClone(template)
+    local clone = template:Clone()
     clone.Parent = game.Workspace
     clone.Name = "CLONE:" .. clone.Name 
     clone:MoveTo(Vector3.new(0,0,0))
 end
 
-function EchoService:Start()
-    print("Echo Server Start")
+local CatService = {Client = {}}
+local BaseCat = 0
+
+
+function CatService:Start()
     local npc_directory = game.Workspace['NPC'] --put the name of the folder here
     local npcs = npc_directory:GetChildren();
-
     for _,v in pairs(npcs) do
         print (v.name)
         MakeAClone(v)
     end
+end
+
+
+function CatService:Init()
 	
 end
 
 
-function EchoService:Init()
-	
-end
-
-
-return EchoService
+return CatService
