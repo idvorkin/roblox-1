7e55d3f1d34920497550ca29b9ee7b388e2347eb
Igor Dvorkin <idvorkin@gmail.com>
2020-09-07 11:13:54 -0700

    Commit

--- src/Server/Services/CatService.lua
+++ src/Server/Services/CatService.lua
@@ -3,7 +3,11 @@
 -- September 7, 2020
 
 local _ 
-local all_cats 
+local count_cats = 20
+
+-- Can't figure out how to map to in game position, so store it externally
+-- Arguably, this is closer to a view model, so it's more gooder
+
 
 function MakeAClone(template)
     local clone = template:Clone()
@@ -11,37 +15,46 @@ function MakeAClone(template)
     clone.Name = "CLONE:" .. clone.Name 
     return clone
 end
---    clone:MoveTo(start_location)
 
-function MoveRandom(part)
+function randomNegate(x)
+    local dice = math.random()
+    if dice < 0.5 then 
+        return -1*x
+    end
+    return x
+end 
+
+function MoveOneSquareRandom(model)
+    print('moving one')
+    old_pos = model.PrimaryPart.Position
+    local new_x  = old_pos.X + randomNegate(1) 
+    local new_z  = old_pos.Y + randomNegate(1)
+    local new_pos = Vector3.new(new_x,old_pos.y,new_z)
+    print (old_pos)
+    print (new_pos)
+    model:MoveTo(new_pos)
+end
+
+function MoveRandom(model)
+    print('moving random')
     local radius = 100
     local x = math.random(-1*radius, 1*radius)
     local z = math.random(-1*radius, 1*radius)
     local location = Vector3.new(x,0,z)
-    part:MoveTo(location)
+    model:MoveTo(location)
 end
-
-function MoveOneSquareRandom(part)
-    local radius = 100
-    local x = part.position.x
-    local z = part.position.z
-    local location = Vector3.new(x,0,z)
-    part:MoveTo(location)
-end
-
-
 
 
 local CatService = {Client = {}}
-local BaseCat = 0
-
 
 function CatService:Start()
-    -- _ = self.Modules.underscore
     _ = self.Shared.underscore
+    print('CatService:Start')
+    local radius = 100
     local catTemplate = game.Workspace.Templates.Cat --put the name of the folder here
-    local all_cats = _.map(_.range(5), function (i) 
-        return  MakeAClone(catTemplate)
+
+    local all_cats = _.map(_.range(count_cats), function (i) 
+        return MakeAClone(catTemplate) 
     end)
 
     -- Set initial Values
